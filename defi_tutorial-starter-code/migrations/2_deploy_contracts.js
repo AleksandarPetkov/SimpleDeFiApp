18f67cdd2cd08da28fc2e8a8e90da2e0a41de741
unknown <a.petkovpetkov@gmail.com>
2022-01-06 20:00:01 +0200

    DeFi: Deploy all smart-contracts on the block-chain

--- defi_tutorial-starter-code/migrations/2_deploy_contracts.js
+++ defi_tutorial-starter-code/migrations/2_deploy_contracts.js
@@ -1,5 +1,25 @@
 const FarmToken = artifacts.require('FarmToken');
+const DappToken = artifacts.require('DappToken');
+const DaiToken = artifacts.require('DaiToken');
 
-module.exports = function(deployer) {
-  deployer.deploy(FarmToken);
+
+//Puts all of the Smart-Contracts on the Block-Chain
+module.exports = async function (deployer, network, accounts) {
+  //Deploy Dapp Token
+  await deployer.deploy(DappToken);
+  const dappToken = await DappToken.deployed();
+
+  //Deploy Mock Dai Token
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.deployed();
+
+  //Deploy Farm Token
+  await deployer.deploy(FarmToken, dappToken.address, daiToken.address);
+  const farmToken = await FarmToken.deployed();
+
+  //Transferr all Tokens to Farm Token
+  await dappToken.transfer(farmToken.address, '1000000000000000000000000');
+  
+  //Transferr Mock Dai Token to Investor
+  await daiToken.transfer(accounts[1], '1000000000000000000000000');
 };
--- defi_tutorial-starter-code/src/contracts/FarmToken.sol
+++ defi_tutorial-starter-code/src/contracts/FarmToken.sol
@@ -1,5 +1,15 @@
 pragma solidity ^0.5.0;
 
+import "./DaiToken.sol";
+import "./DappToken.sol";
+
 contract FarmToken {
     string public name = "Dapp Token Farm";
+    DaiToken public daiToken;
+    DappToken public dappToken;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
+        daiToken = _daiToken;
+        dappToken = _dappToken;
+    }
 }
